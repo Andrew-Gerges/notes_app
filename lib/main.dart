0180730f751fcfcb4b6d281095850525dc5db3e9
ANDERW <andrew.g3r@gmail>
2025-03-09 17:19:50 +0200

    create custom AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/notes_view_body.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -10,9 +11,11 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark
       ),
+      home: NotesViewBody(),
     );
   }
 }
