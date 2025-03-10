34202a89961823807963575721c165c703115f6e
ANDERW <andrew.g3r@gmail>
2025-03-10 07:05:42 +0200

    create the floating action button

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -6,8 +7,15 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: NotesViewBody(),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.teal.shade900,
+        onPressed: () {},
+        child: Icon(
+          FontAwesomeIcons.plus,
+        ),
+      ),
+      body: const NotesViewBody(),
     );
   }
 }
