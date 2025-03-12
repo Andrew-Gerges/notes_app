4a9e0ef7ee9d78999f07031077f515944b974af4
ANDERW <andrew.g3r@gmail>
2025-03-12 09:00:11 +0200

    prepare the edit notes view

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,67 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: kPrimaryColor,
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 8, top: 8, bottom: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              ListTile(
-                title: Text(
-                  'Flutter Note',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: kPrimaryColor,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8, top: 8, bottom: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ListTile(
+                  title: Text(
+                    'Flutter Note',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 16,
+                    ),
+                    child: Text(
+                      'Flutter Notes Subtitle or Description or Somthing else - Flutter Notes Subtitle or Description or Somthing else',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'asset: lib/assets/fonts/Poppins-Thin.ttf',
+                      ),
+                    ),
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Icon(
+                        FontAwesomeIcons.trash,
+                      ),
+                    ),
+                  ),
                 ),
-                subtitle: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 16,
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 16,
+                    right: 16,
+                    top: 16,
                   ),
                   child: Text(
-                    'Flutter Notes Subtitle or Description or Somthing else - Flutter Notes Subtitle or Description or Somthing else',
+                    'March 10 2025',
                     style: TextStyle(
-                      fontSize: 16,
+                      // fontWeight: FontWeight.w300,
+                      color: Colors.white.withValues(alpha: 0.5),
                       fontFamily: 'asset: lib/assets/fonts/Poppins-Thin.ttf',
                     ),
                   ),
                 ),
-                trailing: IconButton(
-                  onPressed: () {},
-                  icon: Padding(
-                    padding: const EdgeInsets.only(left: 16),
-                    child: Icon(
-                      FontAwesomeIcons.trash,
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 16,
-                  right: 16,
-                  top: 16,
-                ),
-                child: Text(
-                  'March 10 2025',
-                  style: TextStyle(
-                    // fontWeight: FontWeight.w300,
-                    color: Colors.white.withValues(alpha: 0.5),
-                    fontFamily: 'asset: lib/assets/fonts/Poppins-Thin.ttf',
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
